575920111dd94dcc23d92cc3d5f0690ef26e1852
Yurzs <yurzs@icloud.com>
2021-10-12 03:03:08 +0300

    game: fix xor middleware

--- game/game/middleware/xor.py
+++ game/game/middleware/xor.py
@@ -9,7 +9,7 @@ class XORGameMiddleware(Middleware):
 
             temp1 = Int32(0)
             for i in range(0, len(request.data)):
-                temp2 = Int32(request.data[i]) & 255
+                temp2 = Int32(request.data[i]) & 0xFF
                 request.data[i] = Int8(
                     temp2 ^ session.xor_key.incoming_key[i & 15] ^ temp1
                 )
@@ -28,19 +28,19 @@ class XORGameMiddleware(Middleware):
     def after(cls, session, response):
         if session.encryption_enabled:
 
-            temp = Int32(0)
+            temp1 = Int32(0)
 
             for i in range(0, len(response.data)):
-                temp2 = Int32(response.data[i]) & 0xFF
+                temp2 = Int32(response.data[i] & 0xFF)
                 response.data[i] = Int8(
-                    temp2 ^ session.xor_key.outgoing_key[i & 15] ^ temp
+                    temp2 ^ session.xor_key.outgoing_key[i & 15] ^ temp1
                 )
-                temp = response.data[i]
+                temp1 = response.data[i]
 
             old = Int32(session.xor_key.outgoing_key[8]) & 0xFF
-            old |= Int32(session.xor_key.outgoing_key[9] << 8) & 0xFF00
-            old |= Int32(session.xor_key.outgoing_key[10] << 16) & 0xFF0000
-            old |= Int32(session.xor_key.outgoing_key[11] << 24) & 0xFF000000
+            old |= (Int32(session.xor_key.outgoing_key[9]) << 0x08) & 0xFF00
+            old |= (Int32(session.xor_key.outgoing_key[10]) << 0x10) & 0xFF0000
+            old |= (Int32(session.xor_key.outgoing_key[11]) << 0x18) & 0xFF000000
 
             old += Int32(len(response.data))
 
