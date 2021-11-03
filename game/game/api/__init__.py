66d87ab770fbd0a0c76f405e9a4e01f830984dd0
Yurzs <yurzs@icloud.com>
2021-11-04 01:05:24 +0300

    game: move api one level upper

---
+++ game/game/api/__init__.py
@@ -0,0 +1,1 @@
+from . import action, attack, characters, game, minimap, move, say, world
--- game/game/api/http/ping.py
+++
@@ -1,9 +0,0 @@
-import json
-
-from aiohttp.web import Response
-from aiojsonapi import routes
-
-
-@routes.post("/api/ping")
-async def ping(request):
-    return Response(body=json.dumps({"result": {"pong": True}}), content_type="application/json")
